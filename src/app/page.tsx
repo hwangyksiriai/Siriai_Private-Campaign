58e816c5f9e48674fd84cfbc7d809a97a776630b
hwangyksiriai <hwangyksiriai@gmail.com>
2026-08-13 15:12:04 +0900

    Move campaign list below the Collaboration Process section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <Hero />
-        <PublicCampaigns />
         <BrandMarquee />
         <About />
         <HowItWorks />
+        <PublicCampaigns />
       </main>
       <Footer />
     </>
